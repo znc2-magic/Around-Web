3eabbbe51541fec05f812dd3ef3eb327399934ad
Shane <znc2.magic@gmail.com>
2018-02-26 13:34:19 -0800

    Add Tabs style

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,10 @@ const operations = <Button>Extra Action</Button>;
 export class Home extends React.Component {
   render() {
     return (
-      <div>
-        <Tabs tabBarExtraContent={operations}>
-          <TabPane tab="Tab 1" key="1">Content of tab 1</TabPane>
-          <TabPane tab="Tab 2" key="2">Content of tab 2</TabPane>
+        <Tabs tabBarExtraContent={operations} className="main-tabs">
+          <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
+          <TabPane tab="Map" key="2">Content of tab 2</TabPane>
         </Tabs>
-      </div>
     );
   }
 }
