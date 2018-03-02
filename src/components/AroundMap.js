4f61bda03789e441f2061079fd27e8053cf93001
Shane <znc2.magic@gmail.com>
2018-03-01 23:02:35 -0800

    Finalized Drag and Zoom reload

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {POS_KEY} from '../constants';
 import {AroundMarker} from './AroundMarker';
-
 import {withScriptjs, withGoogleMap, GoogleMap} from 'react-google-maps';
 
 class AroundMap extends React.Component {
   reloadMarkers = () => {
     const center = this.map.getCenter();
     const location = {lat: center.lat(), lon: center.lng()};
-    this.props.loadNearbyPosts(location);
+    const range = this.getRange();
+    this.props.loadNearbyPosts(location, range);
+  }
+
+  getRange = () => {
+    const google = window.google;
+    const center = this.map.getCenter();
+    const bounds = this.map.getBounds();
+    if (center && bounds) {
+      const ne = bounds.getNorthEast();
+      const right = new google.maps.LatLng(center.lat(), ne.lng());
+      return 0.000621371192 * google.maps.geometry.spherical.computeDistanceBetween(center, right);
+    }
   }
 
   getMapRef = (map) => {
@@ -25,6 +36,7 @@ class AroundMap extends React.Component {
         defaultZoom={11}
         defaultCenter={{ lat: lat, lng: lon }}
         onDragEnd={this.reloadMarkers}
+        onZoomChanged={this.reloadMarkers}
       >
         {
           this.props.posts ? this.props.posts.map((post) =>
--- src/components/Home.js
+++ src/components/Home.js
@@ -78,14 +78,15 @@ export class Home extends React.Component {
     }
   }
 
-  loadNearbyPosts = (location) => {
+  loadNearbyPosts = (location, radius) => {
     const {lat, lon} = location ? location : JSON.parse(localStorage.getItem(POS_KEY));
+    const range = radius ? radius : 20;
     this.setState({
       loadingPosts: true,
       error: '',
     })
     return $.ajax({
-      url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+      url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=${range}`,
       method: 'GET',
       headers: {
         Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
